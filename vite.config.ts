4dea6cbbb25dae538880858c94e9b9a01b43b58d
luanjunqueira <trevossystem@gmail.com>
2026-02-07 18:07:38 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/portifolio-luan/",
+  base: "/portfolio-luann/",   
   server: {
     host: "::",
     port: 8080,
